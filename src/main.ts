40c47f28f20ff0f813f035fdce652bddcfecabd1
Eduardo Felipe Weber <eduardo-felipe-weber@hotmail.com>
2022-03-13 21:51:33 -0300

    Add vueGtag lib

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import VueGtag from 'vue-gtag'
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(VueGtag, {
+    config: { id: 'G-JFD8REZ0Q6' },
+  })
   .mount('#app')
