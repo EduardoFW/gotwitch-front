7564b70b8e96a6d1a70554e949c6c51af7876961
Eduardo Felipe Weber <eduardo-felipe-weber@hotmail.com>
2022-10-27 08:25:22 -0300

    Install vue-toastification

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import VueGtag from 'vue-gtag'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(Toast)
   .use(VueGtag, {
     config: { id: 'G-JFD8REZ0Q6' },
   })
